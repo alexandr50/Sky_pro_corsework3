5bd340c6d40d5104da97041ae58275a9db897b8b
alexandr50 <dmitrymaslov2016@mail.ru>
2023-02-25 13:18:41 +0300

    рефакторинг

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,8 @@
 import unittest
 from scripts import utils
-from .data.result_data import result_list
+
+RESULT_LIST = [{'id': 863064926, 'state': 'EXECUTED', 'date': '2019-12-08 22:46', 'operationAmount': {'amount': '41096.24', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Открытие вклада', 'to': 'Счет 90424923579946435907'}, {'id': 114832369, 'state': 'EXECUTED', 'date': '2019-12-07 06:17', 'operationAmount': {'amount': '48150.39', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Visa Classic 2842878893689012', 'to': 'Счет 35158586384610753655'}, {'id': 560813069, 'state': 'CANCELED', 'date': '2019-12-03 04:27', 'operationAmount': {'amount': '17628.50', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод с карты на карту', 'from': 'MasterCard 1796816785869527', 'to': 'Visa Classic 7699855375169288'}, {'id': 154927927, 'state': 'EXECUTED', 'date': '2019-11-19 09:22', 'operationAmount': {'amount': '30153.72', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод организации', 'from': 'Maestro 7810846596785568', 'to': 'Счет 43241152692663622869'}, {'id': 482520625, 'state': 'EXECUTED', 'date': '2019-11-13 17:38', 'operationAmount': {'amount': '62814.53', 'currency': {'name': 'руб.', 'code': 'RUB'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 38611439522855669794', 'to': 'Счет 46765464282437878125'}]
+
 
 
 class TestUtils(unittest.TestCase):
@@ -21,7 +23,7 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(utils.correct_number_card(None), "")
 
     def test_sorted_func_for_date(self):
-        self.assertEqual(utils.sorted_func_for_date('tests/data/test.json'), result_list)
+        self.assertEqual(utils.sorted_func_for_date('tests/data/test.json'), RESULT_LIST)
 
 
 if __name__ == '__main__':
